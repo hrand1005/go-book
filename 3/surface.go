fbcfaca35ee71219008482e6bd4eede0030eb190
Herbie <hjrand1005@gmail.com>
2022-05-30 00:33:13 -0400

    ADD: use writer for svg dump

--- 3/surface.go
+++ 3/surface.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+    "flag"
     "fmt"
+    "io"
+    "log"
     "math"
+    "os"
 )
 
 const (
@@ -16,10 +20,45 @@ const (
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) 
 
+var outfile = flag.String("outfile", "", "File to write svg data.")
+
 func main() {
-    fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+    flag.Parse()
+
+    if outfile == nil {
+        shapeName(os.Stdout)
+        return
+    }
+
+    f, err := os.Create(*outfile)
+    if err != nil {
+        log.Printf("Failed to create outfile, printing output.")
+        shapeName(os.Stdout)
+        return
+    }
+    defer f.Close()
+
+    shapeName(f)
+
+    /*
+    if outfile == "" {
+        shapeName(os.Stdout)
+    } else {
+        // create file
+        f, err := os.Create(outfile)
+        if err != nil {
+            log.Printf("Failed to create outfile, using os.Stdout")
+        }
+    }
+    */
+}
+
+func shapeName(w io.Writer) {
+    header := fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
         "style='stroke: grey; fill: white; stroke-width: 0.7' "+
         "width='%d' height='%d'>", width, height)
+    w.Write([]byte(header)) 
+
     for i := 0; i < cells; i++ {
         for j := 0; j < cells; j++ {
             ax, ay := corner(i+1, j)
@@ -27,12 +66,13 @@ func main() {
             cx, cy := corner(i, j+1)
             dx, dy := corner(i+1, j+1)
             if checkValidFloats([]float64{ax, ay, bx, by, cx, cy, dx, dy}) {
-                fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g'/>\n", 
+                line := fmt.Sprintf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g'/>\n", 
                     ax, ay, bx, by, cx, cy, dx, dy)
+                w.Write([]byte(line))
             } 
         }
     }
-    fmt.Println("</svg>")
+    w.Write([]byte("</svg>"))
 }
 
 func corner(i, j int) (float64, float64) {
